06237eb39c6b55a77188cd4d1cfc6c5f1e50db44
Raven <wu_bin3@dahuatech.com>
2023-09-08 11:39:27 +0800

    change trancing logger to localtime

--- src/lib.rs
+++ src/lib.rs
@@ -104,7 +104,7 @@ pub fn init_tracing_logger(
     debug: bool,
     default: &str,
 ) -> tracing_appender::non_blocking::WorkerGuard {
-    use tracing_subscriber::prelude::*;
+    use tracing_subscriber::{fmt::time::OffsetTime, prelude::*};
 
     let file_appender = tracing_appender::rolling::daily(log_dir, format!("{pkg_name}.log"));
     let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
@@ -113,14 +113,22 @@ pub fn init_tracing_logger(
     } else {
         default.to_owned()
     };
+
+    let timer = OffsetTime::new(
+        time::macros::offset!(+8),
+        time::macros::format_description!(
+            "[year]-[month]-[day]T[hour]:[minute]:[second]"
+        ),
+    );
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| log_level.into()),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer().with_timer(timer.clone()))
         .with(
             tracing_subscriber::fmt::layer()
+                .with_timer(timer)
                 .with_writer(non_blocking)
                 .with_ansi(false),
         )
