074d488a50c26bdbb3e90c7aefdd2c284cf9f46b
Raven <wu_bin3@dahuatech.com>
2025-01-14 13:45:09 +0800

    fix BINARY_PKG_NAME env should come from compile time

--- src/temp.rs
+++ src/temp.rs
@@ -1,13 +1,13 @@
 use dirs::cache_dir;
-use std::{
-    env::var,
-    path::{Path, PathBuf},
-};
+use std::path::PathBuf;
 
 pub fn temp_dir() -> PathBuf {
-    let env_p = var("BINARY_PKG_NAME");
-    let env_p = env_p.as_ref().map(|s| s.as_str());
+    let env_p = env!("BINARY_PKG_NAME");
+    let p = if env_p.is_empty() {
+        env!("CARGO_PKG_NAME")
+    } else {
+        env_p
+    };
 
-    let p = Path::new(env_p.unwrap_or(env!("CARGO_PKG_NAME")));
     cache_dir().unwrap_or_else(std::env::temp_dir).join(p)
 }
