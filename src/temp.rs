7f9041b4ad14dc9c010050e5dbfcf8fd8001f6e3
Raven <wu_bin3@dahuatech.com>
2025-02-27 15:42:08 +0800

    return temp_dir before return

--- src/temp.rs
+++ src/temp.rs
@@ -1,8 +1,17 @@
 use dirs::cache_dir;
-use std::path::PathBuf;
+use log::error;
+use std::{fs::create_dir_all, path::PathBuf};
 
 pub fn temp_dir() -> PathBuf {
     let env_p = option_env!("BINARY_PKG_NAME");
     let p = env_p.unwrap_or(env!("CARGO_PKG_NAME"));
-    cache_dir().unwrap_or_else(std::env::temp_dir).join(p)
+    let path = cache_dir().unwrap_or_else(std::env::temp_dir).join(p);
+
+    if !path.exists() {
+        if let Err(e) = create_dir_all(&path) {
+            error!("create temp dir {} error: {:?}", path.display(), e);
+        }
+    }
+
+    path
 }
